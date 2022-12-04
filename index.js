543dd04c985600575f8503777e5bc8b6456e74f6
MR RAKIB <rakibulislamsofficial@gmail.com>
2022-12-04 22:26:12 +0600

    mongodb ready to use

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const http = require('http');
@@ -24,6 +25,23 @@ io.on("connection", (socket) => {
     console.log(socket.id);
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvyuz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        console.log('Mongodb is connected');
+    }
+    catch (err) {
+
+    }
+    finally {
+        // client.close();
+    }
+}
+
+run().catch(console.dir)
 
 
 
