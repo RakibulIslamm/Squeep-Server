89f92839734efc6b924d2977cb614b6c77d3e24e
MR RAKIB <rakibulislamsofficial@gmail.com>
2022-12-04 21:40:49 +0600

    socket io setup done

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const http = require('http')
-const expressServer = http.createServer(app);
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+const expressServer = http.createServer(app);
+const io = new Server(expressServer, {
+    cors: {
+        origin: ['http://localhost:3000']
+    }
+});
+
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Squeep server!');
-})
-
-
+});
 
 expressServer.listen(port, () => console.log('Server running at port ' + port));
