7cf714eb6547766ae42cc1cce2280423e2d255b3
MR RAKIB <rakibulislamsofficial@gmail.com>
2022-12-06 19:58:16 +0600

    add user, find friend and all requested friend api created

--- index.js
+++ index.js
@@ -32,6 +32,52 @@ const run = async () => {
     try {
         await client.connect();
         console.log('Mongodb is connected');
+        const db = client.db("chat_application_DB");
+        const usersCollection = db.collection('users');
+        const friendsCollection = db.collection('friends');
+
+        // Add user api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // find friend api
+        app.get('/find-people', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const currentUser = await usersCollection.findOne(query);
+            if (currentUser) {
+                const friends = await friendsCollection.find().toArray();
+                const neFilter = friends.filter(user => user.friendship.includes(currentUser.email)).map(user => user.friendship.filter(eml => eml !== currentUser.email)).map(email => email[0]);
+                const users = await usersCollection.find({ email: { $nin: [...neFilter, email] } }).toArray();
+                const result = users.map(user => {
+                    return {
+                        name: user.name,
+                        email: user.email,
+                        username: user.username,
+                        _id: user._id,
+                        img: user.img
+                    }
+                });
+                res.send(result);
+            }
+            else {
+                res.send({ code: 404 });
+            }
+        });
+
+        //  Get all requested friends
+        app.get('/sr-friends', async (req, res) => {
+            const { email } = req.query;
+            const friends = await friendsCollection.find().toArray();
+            const result = friends.filter(friend => friend.friendship.includes(email)).filter(f => f.status === 'pending');
+            res.send(result);
+        })
+
+
+
     }
     catch (err) {
 
@@ -42,8 +88,6 @@ const run = async () => {
 }
 
 run().catch(console.dir)
-
-
 
 app.get('/', (req, res) => {
     res.send('Hello from Squeep server!');
