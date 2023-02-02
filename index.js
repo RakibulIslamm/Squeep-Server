7eb40d9f6918a201ed07c1bff7abae8478a77cf7
MR RAKIB <rakibulislamsofficial@gmail.com>
2023-02-02 20:20:55 +0600

    cors option added

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const { Server } = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const expressServer = http.createServer(app);
